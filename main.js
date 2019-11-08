60dbc76d1ba5795fdab726ffe63181409ff20d97
Jenz Delmar <wmb@Jenz-Delmars-MacBook-Pro.local>
2019-11-07 16:12:29 -0800

    updates on website

--- main.js
+++ main.js
@@ -1,15 +1,24 @@
   
+
+
         const message = document.querySelector(".message");
         const score = document.querySelector(".score");
         const buttons = document.querySelectorAll("button");
         let winner = [0, 0];
- 
+ // score array
+
+
+// each button
         for (let i = 0; i < buttons.length; i++) {
             buttons[i].addEventListener("click", playGame);
         }
- 
+ // assigns funtion for click event
+
         function playGame(e) {
+// dom object button clicked
+//                                  button  contents    
             let playerSelection = e.target.innerText;
+                                 
             let computerSelection = Math.random();
             if (computerSelection < 0.34) {
                 computerSelection = "Rock";
@@ -20,11 +29,14 @@
             else {
                 computerSelection = "Scissors";
             }
+
             let result = checkWinner(playerSelection, computerSelection);
+
             if (result == "Player") {
                 result += " wins!";
                 winner[0]++;
             }
+
             else if (result == "Computer") {
                 result += " wins!";
                 winner[1]++;
@@ -71,3 +83,36 @@
         }
    
     
+
+// let humanScore = 0;
+// let computerScore = 0;
+
+// function play(humanPlay, computerPlay){
+//     console.group('play');
+
+//     console.log(humanPlay);
+//     console.log(computerPlay);
+
+// if(humanPlay === computerPlay){
+//     console.log('you tied. :|');
+// } else if (humanPlay === 'paper' && computerPlay === 'rock' ||
+//            humanPlay === 'rock' && computerPlay  === 'scissors' ||
+//            humanPlay ===  'scissors' && computerPlay === 'paper'){
+//             console.log('you win!');
+//             humanScore += 1;
+// } else {
+//     console.log('You lose');
+//     computerScore += 1;
+// }
+
+// console.log(humanScore);
+// console.log(computerScore);
+// }
+
+// play('rock', 'scissors');
+// play('rock', 'paper');
+// play('scissors', 'paper');
+// play('scissors', 'rock');
+// play('paper', 'scissors');
+// play('paper', 'rock');
+
